136993c9450d0562462b0b5e042f51ec05467628
KimGiWoon <rldns20704@naver.com>
2026-06-07 00:30:03 +0900

    [Feat] Add Example_128
    128번 파일 위치 확인

---
+++ Form_Example_128.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// 솔루션 및 프로젝트 파일의 위치
+namespace C__200_Example_WinForm
+{
+    public partial class Form_Example_128 : Form
+    {
+        public Form_Example_128()
+        {
+            InitializeComponent();
+        }
+
+        // 솔루션 이름에 마우스 우클릭
+        // 파일 탐색기에서 열기 선택
+        // 프로젝트의 이름과 폴더 확인
+        // 다른 사람에게 소스를 보내고 싶을 땐 bin, obj폴더를 삭제하고 압축하여 전달
+    }
+}
