5b01e487c81887175808c2c241aa71622c00fb96
KimGiWoon <rldns20704@naver.com>
2026-06-07 22:48:19 +0900

    [Feat] Add Example_139
    139번 WinForm 예제 문제 추가

---
+++ Form_Example_139.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace C__200_Example_WinForm
+{
+    partial class Form_Example_139
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.scrR = new System.Windows.Forms.HScrollBar();
+            this.scrG = new System.Windows.Forms.HScrollBar();
+            this.scrB = new System.Windows.Forms.HScrollBar();
+            this.txtR = new System.Windows.Forms.TextBox();
+            this.txtG = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(52, 44);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(435, 205);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 278);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Red";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 331);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Green";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 380);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Blue";
+            // 
+            // scrR
+            // 
+            this.scrR.Location = new System.Drawing.Point(106, 268);
+            this.scrR.Maximum = 255;
+            this.scrR.Name = "scrR";
+            this.scrR.Size = new System.Drawing.Size(290, 30);
+            this.scrR.TabIndex = 4;
+            this.scrR.Scroll += new System.Windows.Forms.ScrollEventHandler(this.scr_Scroll);
+            // 
+            // scrG
+            // 
+            this.scrG.Location = new System.Drawing.Point(106, 319);
+            this.scrG.Maximum = 255;
+            this.scrG.Name = "scrG";
+            this.scrG.Size = new System.Drawing.Size(290, 30);
+            this.scrG.TabIndex = 5;
+            this.scrG.Scroll += new System.Windows.Forms.ScrollEventHandler(this.scr_Scroll);
+            // 
+            // scrB
+            // 
+            this.scrB.Location = new System.Drawing.Point(106, 370);
+            this.scrB.Maximum = 255;
+            this.scrB.Name = "scrB";
+            this.scrB.Size = new System.Drawing.Size(290, 30);
+            this.scrB.TabIndex = 6;
+            this.scrB.Scroll += new System.Windows.Forms.ScrollEventHandler(this.scr_Scroll);
+            // 
+            // txtR
+            // 
+            this.txtR.Location = new System.Drawing.Point(420, 272);
+            this.txtR.Name = "txtR";
+            this.txtR.Size = new System.Drawing.Size(67, 21);
+            this.txtR.TabIndex = 7;
+            this.txtR.TextChanged += new System.EventHandler(this.txt_TextChanged);
+            // 
+            // txtG
+            // 
+            this.txtG.Location = new System.Drawing.Point(420, 326);
+            this.txtG.Name = "txtG";
+            this.txtG.Size = new System.Drawing.Size(67, 21);
+            this.txtG.TabIndex = 8;
+            this.txtG.TextChanged += new System.EventHandler(this.txt_TextChanged);
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(420, 374);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(67, 21);
+            this.txtB.TabIndex = 9;
+            this.txtB.TextChanged += new System.EventHandler(this.txt_TextChanged);
+            // 
+            // Form_Example_139
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 450);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtG);
+            this.Controls.Add(this.txtR);
+            this.Controls.Add(this.scrB);
+            this.Controls.Add(this.scrG);
+            this.Controls.Add(this.scrR);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form_Example_139";
+            this.Text = "Form_Example_139";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.HScrollBar scrR;
+        private System.Windows.Forms.HScrollBar scrG;
+        private System.Windows.Forms.HScrollBar scrB;
+        private System.Windows.Forms.TextBox txtR;
+        private System.Windows.Forms.TextBox txtG;
+        private System.Windows.Forms.TextBox txtB;
+    }
+}
---
+++ Form_Example_139.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// 스크롤바로 RGB 컬러 조정
+namespace C__200_Example_WinForm
+{
+    public partial class Form_Example_139 : Form
+    {
+        public Form_Example_139()
+        {
+            InitializeComponent();
+
+            Init();
+        }
+
+        private void Init()
+        {
+            this.BackColor = Color.LightSteelBlue;
+            panel1.BackColor = Color.FromArgb(0, 0, 0); // RGB컬러
+            txtR.Text = 0.ToString();
+            txtG.Text = 0.ToString();
+            txtB.Text = 0.ToString();
+
+            scrR.Maximum = 255 + 9; // LargeChange의 값인 10을 고려하여 실제 최대값 255가 나오도록 설정 (최대값 : Maximum - LargeChange + 1)
+            scrG.Maximum = 255 + 9;
+            scrB.Maximum = 255 + 9;
+
+            this.CenterToScreen();
+        }
+
+        // 스크롤 변화
+        private void scr_Scroll(object sender, ScrollEventArgs e)
+        {
+            txtR.Text = scrR.Value.ToString();
+            txtG.Text = scrG.Value.ToString();
+            txtB.Text = scrB.Value.ToString();
+            panel1.BackColor = Color.FromArgb(scrR.Value, scrG.Value, scrB.Value);
+        }
+
+        // Text Box 변화
+        private void txt_TextChanged(object sender, EventArgs e)
+        {
+            if (txtR.Text != "" && txtG.Text != "" && txtB.Text != "")
+            {
+                scrR.Value = int.Parse(txtR.Text);
+                scrG.Value = int.Parse(txtG.Text);
+                scrB.Value = int.Parse(txtB.Text);
+                panel1.BackColor = Color.FromArgb(scrR.Value, scrG.Value, scrB.Value);
+            }
+        }
+    }
+}
