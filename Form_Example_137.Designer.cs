42b0f544a7240676810bbc4eb33f18919327c09c
KimGiWoon <rldns20704@naver.com>
2026-06-07 21:20:39 +0900

    [Feat] Add Example_137
    137번 WinForm 예제 문제 추가

---
+++ Form_Example_137.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace C__200_Example_WinForm
+{
+    partial class Form_Example_137
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(26, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(217, 186);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "성적입력";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(291, 30);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(217, 130);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "결과";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(429, 186);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(79, 36);
+            this.btnCalc.TabIndex = 2;
+            this.btnCalc.Text = "계산";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // Form_Example_137
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 249);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form_Example_137";
+            this.Text = "Form_Example_137";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnCalc;
+    }
+}
---
+++ Form_Example_137.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+// 성적계산기
+namespace C__200_Example_WinForm
+{
+    public partial class Form_Example_137 : Form
+    {
+        private TextBox txtScore1 = null;
+        private TextBox txtScore2 = null;
+        private TextBox txtScore3 = null;
+        private TextBox txtTotalScore = null;
+        private TextBox txtAverage = null;
+
+        private int maxSub = 3;
+
+        public Form_Example_137()
+        {
+            InitializeComponent();
+
+            Init();
+        }
+
+        private void Init()
+        {
+            txtScore1 = CreateTextBox("txtScore1", "", 70, 40, false);
+            txtScore2 = CreateTextBox("txtScore2", "", 70, 80, false);
+            txtScore3 = CreateTextBox("txtScore3", "", 70, 120, false);
+
+            groupBox1.Controls.Add(CreateLabel("lblSubject1", "국어", 20, 40));
+            groupBox1.Controls.Add(CreateLabel("lblSubject2", "수학", 20, 80));
+            groupBox1.Controls.Add(CreateLabel("lblSubject3", "영어", 20, 120));
+            groupBox1.Controls.Add(txtScore1);
+            groupBox1.Controls.Add(txtScore2);
+            groupBox1.Controls.Add(txtScore3);
+
+            txtTotalScore = CreateTextBox("txtTotalScore", "", 70, 40, true);
+            txtAverage = CreateTextBox("txtAverage", "", 70, 80, true);
+
+            groupBox2.Controls.Add(CreateLabel("lblTotal", "총점", 20, 40));
+            groupBox2.Controls.Add(CreateLabel("lblAverage", "평균", 20, 80));
+            groupBox2.Controls.Add(txtTotalScore);
+            groupBox2.Controls.Add(txtAverage);
+
+            this.CenterToScreen();
+        }
+
+        // 라벨 생성
+        private Label CreateLabel(string name, string text, int x, int y)
+        {
+            Label label = new Label();
+            label.Name = name;
+            label.Text = text;
+            label.Location = new Point(x, y);
+            label.AutoSize = false;
+            label.Size = new Size(30, 20);
+            label.TextAlign = ContentAlignment.MiddleCenter;
+
+            return label;
+        }
+
+        // 텍스트 박스 생성
+        private TextBox CreateTextBox(string name, string text, int x, int y, bool read)
+        {
+            TextBox textBox = new TextBox();
+            textBox.Name = name;
+            textBox.Text = text;
+            textBox.Location = new Point(x, y);
+            textBox.BorderStyle = BorderStyle.FixedSingle;
+            textBox.ReadOnly = read;
+            textBox.TextAlign = HorizontalAlignment.Center;
+
+            return textBox;
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            int totalScore = 0;
+            double scoreAverage = 0;
+
+            if (txtScore1.Text == "" || txtScore2.Text == "" || txtScore3.Text == "")
+            {
+                MessageBox.Show("점수를 모두 입력하세요");
+                return;
+            }
+
+            totalScore = int.Parse(txtScore1.Text) + int.Parse(txtScore2.Text) + int.Parse(txtScore3.Text);
+            scoreAverage = totalScore / maxSub;
+
+            txtTotalScore.Text = totalScore.ToString();
+            txtAverage.Text = scoreAverage.ToString("F1");
+        }
+    }
+}
