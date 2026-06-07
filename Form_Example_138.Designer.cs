8fe17219d28ba12547bea590a82b017e468a58ea
KimGiWoon <rldns20704@naver.com>
2026-06-07 21:59:17 +0900

    [Feat] Add Example_138
    138번 WinForm 예제 문제 추가

---
+++ Form_Example_138.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace C__200_Example_WinForm
+{
+    partial class Form_Example_138
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnRegistration = new System.Windows.Forms.Button();
+            this.txtDate = new System.Windows.Forms.MaskedTextBox();
+            this.txtAddr = new System.Windows.Forms.MaskedTextBox();
+            this.txtMail = new System.Windows.Forms.MaskedTextBox();
+            this.txtPhone = new System.Windows.Forms.MaskedTextBox();
+            this.txtId = new System.Windows.Forms.MaskedTextBox();
+            this.txtEmail = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "입사일";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "우편번호";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "주소";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "휴대폰 번호";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(43, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "주민등록번호";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(43, 175);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 12);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "이메일";
+            // 
+            // btnRegistration
+            // 
+            this.btnRegistration.Location = new System.Drawing.Point(132, 209);
+            this.btnRegistration.Name = "btnRegistration";
+            this.btnRegistration.Size = new System.Drawing.Size(87, 31);
+            this.btnRegistration.TabIndex = 12;
+            this.btnRegistration.Text = "등록";
+            this.btnRegistration.UseVisualStyleBackColor = true;
+            this.btnRegistration.Click += new System.EventHandler(this.btnRegistration_Click);
+            // 
+            // txtDate
+            // 
+            this.txtDate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDate.Location = new System.Drawing.Point(144, 45);
+            this.txtDate.Mask = "0000-00-00";
+            this.txtDate.Name = "txtDate";
+            this.txtDate.Size = new System.Drawing.Size(93, 21);
+            this.txtDate.TabIndex = 13;
+            this.txtDate.ValidatingType = typeof(System.DateTime);
+            // 
+            // txtAddr
+            // 
+            this.txtAddr.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtAddr.Location = new System.Drawing.Point(144, 69);
+            this.txtAddr.Mask = "99999";
+            this.txtAddr.Name = "txtAddr";
+            this.txtAddr.Size = new System.Drawing.Size(93, 21);
+            this.txtAddr.TabIndex = 14;
+            this.txtAddr.ValidatingType = typeof(int);
+            // 
+            // txtMail
+            // 
+            this.txtMail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtMail.Location = new System.Drawing.Point(144, 94);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(235, 21);
+            this.txtMail.TabIndex = 15;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPhone.Location = new System.Drawing.Point(144, 119);
+            this.txtPhone.Mask = "000-9000-0000";
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(93, 21);
+            this.txtPhone.TabIndex = 16;
+            // 
+            // txtId
+            // 
+            this.txtId.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtId.Location = new System.Drawing.Point(144, 145);
+            this.txtId.Mask = "000000-0000000";
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(92, 21);
+            this.txtId.TabIndex = 17;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtEmail.Location = new System.Drawing.Point(144, 170);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(161, 21);
+            this.txtEmail.TabIndex = 18;
+            // 
+            // Form_Example_138
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 266);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.txtAddr);
+            this.Controls.Add(this.txtDate);
+            this.Controls.Add(this.btnRegistration);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form_Example_138";
+            this.Text = "Form_Example_138";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnRegistration;
+        private System.Windows.Forms.MaskedTextBox txtDate;
+        private System.Windows.Forms.MaskedTextBox txtAddr;
+        private System.Windows.Forms.MaskedTextBox txtMail;
+        private System.Windows.Forms.MaskedTextBox txtPhone;
+        private System.Windows.Forms.MaskedTextBox txtId;
+        private System.Windows.Forms.MaskedTextBox txtEmail;
+    }
+}
---
+++ Form_Example_138.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// MaskedTextBox (마스크 텍스트 박스)
+namespace C__200_Example_WinForm
+{
+    public partial class Form_Example_138 : Form
+    {
+        public Form_Example_138()
+        {
+            InitializeComponent();
+
+            this.CenterToScreen();
+        }
+
+        private void btnRegistration_Click(object sender, EventArgs e)
+        {
+            string str;
+
+            str = "입사일: " + txtDate.Text + "\n";
+            str += "우편번호: " + txtMail.Text + "\n";
+            str += "주소: " + txtAddr.Text + "\n";
+            str += "휴대폰 번호: " + txtPhone.Text + "\n";
+            str += "주민등록번호: " + txtId.Text + "\n";
+            str += "이메일: " + txtEmail.Text + "\n";
+
+            MessageBox.Show(str, "개인정보");
+        }
+    }
+}
