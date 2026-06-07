51804c3849eac9e2f3b99811ba6fbfacfce0bd9a
KimGiWoon <rldns20704@naver.com>
2026-06-07 15:00:44 +0900

    [Feat] Add Example_133
    133번 WinForm 예제 문제 추가

---
+++ Form_Example_133.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace C__200_Example_WinForm
+{
+    partial class Form_Example_133
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHello = new System.Windows.Forms.Button();
+            this.lblText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnHello
+            // 
+            this.btnHello.Font = new System.Drawing.Font("굴림", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnHello.Location = new System.Drawing.Point(52, 194);
+            this.btnHello.Name = "btnHello";
+            this.btnHello.Size = new System.Drawing.Size(142, 40);
+            this.btnHello.TabIndex = 0;
+            this.btnHello.Text = "인사하기";
+            this.btnHello.UseVisualStyleBackColor = true;
+            this.btnHello.Click += new System.EventHandler(this.btnHello_Click);
+            // 
+            // lblText
+            // 
+            this.lblText.Font = new System.Drawing.Font("굴림", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblText.Location = new System.Drawing.Point(26, 68);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(192, 41);
+            this.lblText.TabIndex = 1;
+            this.lblText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form_Example_133
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(248, 272);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.btnHello);
+            this.Name = "Form_Example_133";
+            this.Text = "Form_Example_133";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHello;
+        private System.Windows.Forms.Label lblText;
+    }
+}
---
+++ Form_Example_133.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Flag를 이용한 이벤트 처리
+namespace C__200_Example_WinForm
+{
+    public partial class Form_Example_133 : Form
+    {
+        private bool _flag = false;
+
+        public Form_Example_133()
+        {
+            InitializeComponent();
+        }
+
+        private bool GetFlag()
+        {
+            return _flag;
+        }
+
+        private void SetFlag(bool value)
+        {
+            _flag = value;
+        }
+
+        private void btnHello_Click(object sender, EventArgs e)
+        {
+            if (GetFlag() == false)
+            {
+                SetFlag(true);
+                lblText.Text = "안녕하세요~!";
+                btnHello.Text = "초기화";
+            }
+            else
+            {
+                SetFlag(false);
+                lblText.Text = "";
+                btnHello.Text = "인사하기";
+            }
+        }
+    }
+}
