3cf4664fdc4271941e744dcb3b764094aae1da49
KimGiWoon <rldns20704@naver.com>
2026-06-14 23:14:53 +0900

    [Feat] Add Example_144
    144번 WinForm 예제 문제 추가

---
+++ Form_Example_144.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace C__200_Example_WinForm
+{
+    partial class Form_Example_144
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvMain = new System.Windows.Forms.ListView();
+            this.rdoDetails = new System.Windows.Forms.RadioButton();
+            this.rdoList = new System.Windows.Forms.RadioButton();
+            this.rdoSmallIcon = new System.Windows.Forms.RadioButton();
+            this.rdoBigIcon = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lvMain
+            // 
+            this.lvMain.HideSelection = false;
+            this.lvMain.Location = new System.Drawing.Point(25, 67);
+            this.lvMain.Name = "lvMain";
+            this.lvMain.Size = new System.Drawing.Size(456, 201);
+            this.lvMain.TabIndex = 0;
+            this.lvMain.UseCompatibleStateImageBehavior = false;
+            this.lvMain.SelectedIndexChanged += new System.EventHandler(this.lvMain_SelectedIndexChanged);
+            // 
+            // rdoDetails
+            // 
+            this.rdoDetails.AutoSize = true;
+            this.rdoDetails.Location = new System.Drawing.Point(61, 33);
+            this.rdoDetails.Name = "rdoDetails";
+            this.rdoDetails.Size = new System.Drawing.Size(59, 16);
+            this.rdoDetails.TabIndex = 1;
+            this.rdoDetails.TabStop = true;
+            this.rdoDetails.Text = "자세히";
+            this.rdoDetails.UseVisualStyleBackColor = true;
+            this.rdoDetails.CheckedChanged += new System.EventHandler(this.rdoDetails_CheckedChanged);
+            // 
+            // rdoList
+            // 
+            this.rdoList.AutoSize = true;
+            this.rdoList.Location = new System.Drawing.Point(159, 33);
+            this.rdoList.Name = "rdoList";
+            this.rdoList.Size = new System.Drawing.Size(59, 16);
+            this.rdoList.TabIndex = 2;
+            this.rdoList.TabStop = true;
+            this.rdoList.Text = "리스트";
+            this.rdoList.UseVisualStyleBackColor = true;
+            this.rdoList.CheckedChanged += new System.EventHandler(this.rdoList_CheckedChanged);
+            // 
+            // rdoSmallIcon
+            // 
+            this.rdoSmallIcon.AutoSize = true;
+            this.rdoSmallIcon.Location = new System.Drawing.Point(257, 33);
+            this.rdoSmallIcon.Name = "rdoSmallIcon";
+            this.rdoSmallIcon.Size = new System.Drawing.Size(87, 16);
+            this.rdoSmallIcon.TabIndex = 3;
+            this.rdoSmallIcon.TabStop = true;
+            this.rdoSmallIcon.Text = "작은 아이콘";
+            this.rdoSmallIcon.UseVisualStyleBackColor = true;
+            this.rdoSmallIcon.CheckedChanged += new System.EventHandler(this.rdoSmallIcon_CheckedChanged);
+            // 
+            // rdoBigIcon
+            // 
+            this.rdoBigIcon.AutoSize = true;
+            this.rdoBigIcon.Location = new System.Drawing.Point(371, 33);
+            this.rdoBigIcon.Name = "rdoBigIcon";
+            this.rdoBigIcon.Size = new System.Drawing.Size(75, 16);
+            this.rdoBigIcon.TabIndex = 4;
+            this.rdoBigIcon.TabStop = true;
+            this.rdoBigIcon.Text = "큰 아이콘";
+            this.rdoBigIcon.UseVisualStyleBackColor = true;
+            this.rdoBigIcon.CheckedChanged += new System.EventHandler(this.rdoBigIcon_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(242, 296);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Selected";
+            // 
+            // lblText
+            // 
+            this.lblText.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblText.Location = new System.Drawing.Point(302, 289);
+            this.lblText.Name = "lblText";
+            this.lblText.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblText.Size = new System.Drawing.Size(179, 27);
+            this.lblText.TabIndex = 6;
+            this.lblText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form_Example_144
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 337);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rdoBigIcon);
+            this.Controls.Add(this.rdoSmallIcon);
+            this.Controls.Add(this.rdoList);
+            this.Controls.Add(this.rdoDetails);
+            this.Controls.Add(this.lvMain);
+            this.Name = "Form_Example_144";
+            this.Text = "Form_Example_144";
+            this.Load += new System.EventHandler(this.Form_Example_144_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvMain;
+        private System.Windows.Forms.RadioButton rdoDetails;
+        private System.Windows.Forms.RadioButton rdoList;
+        private System.Windows.Forms.RadioButton rdoSmallIcon;
+        private System.Windows.Forms.RadioButton rdoBigIcon;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblText;
+    }
+}
---
+++ Form_Example_144.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// 리스트뷰를 이용한 상품 리스트
+namespace C__200_Example_WinForm
+{
+    public partial class Form_Example_144 : Form
+    {
+        public Form_Example_144()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Example_144_Load(object sender, EventArgs e)
+        {
+            lvMain.GridLines = true;
+            lvMain.FullRowSelect = true;
+
+            lvMain.Columns.Add("제품명", 150);
+            lvMain.Columns.Add("단가", 100, HorizontalAlignment.Right);
+            lvMain.Columns.Add("수량", 70, HorizontalAlignment.Right);
+
+            int remainWidth = lvMain.ClientSize.Width - 150 - 100 - 70 -5;
+            lvMain.Columns.Add("금액", remainWidth, HorizontalAlignment.Right);
+
+            ListViewItem item1 = new ListViewItem("Access", 0);
+            ListViewItem item2 = new ListViewItem("Excel", 1);
+            ListViewItem item3 = new ListViewItem("PowerPoint", 2);
+            ListViewItem item4 = new ListViewItem("Word", 3);
+
+            item1.SubItems.Add("22,000");
+            item1.SubItems.Add("30");
+            item1.SubItems.Add("660,000");
+
+            item2.SubItems.Add("33,000");
+            item2.SubItems.Add("50");
+            item2.SubItems.Add("1,650,000");
+
+            item3.SubItems.Add("11,000");
+            item3.SubItems.Add("50");
+            item3.SubItems.Add("550,000");
+
+            item4.SubItems.Add("22,000");
+            item4.SubItems.Add("30");
+            item4.SubItems.Add("660,000");
+
+            lvMain.Items.AddRange(new ListViewItem[] { item1, item2, item3, item4 });
+
+            ImageList sImageList = new ImageList();
+            ImageList lImageList = new ImageList();
+
+            sImageList.ImageSize = new Size(24, 24);
+            lImageList.ImageSize = new Size(64, 64);
+
+            sImageList.Images.Add(Bitmap.FromFile(@"C:/Users/rldns/OneDrive/바탕 화면/Image/access.png"));
+            sImageList.Images.Add(Bitmap.FromFile(@"C:/Users/rldns/OneDrive/바탕 화면/Image/excel.png"));
+            sImageList.Images.Add(Bitmap.FromFile(@"C:/Users/rldns/OneDrive/바탕 화면/Image/ppt.png"));
+            sImageList.Images.Add(Bitmap.FromFile(@"C:/Users/rldns/OneDrive/바탕 화면/Image/word.png"));
+
+            lImageList.Images.Add(Bitmap.FromFile(@"C:/Users/rldns/OneDrive/바탕 화면/Image/access.png"));
+            lImageList.Images.Add(Bitmap.FromFile(@"C:/Users/rldns/OneDrive/바탕 화면/Image/excel.png"));
+            lImageList.Images.Add(Bitmap.FromFile(@"C:/Users/rldns/OneDrive/바탕 화면/Image/ppt.png"));
+            lImageList.Images.Add(Bitmap.FromFile(@"C:/Users/rldns/OneDrive/바탕 화면/Image/word.png"));
+        }
+
+        private void rdoDetails_CheckedChanged(object sender, EventArgs e)
+        {
+            lvMain.View = View.Details;
+        }
+
+        private void rdoList_CheckedChanged(object sender, EventArgs e)
+        {
+            lvMain.View = View.List;
+        }
+
+        private void rdoSmallIcon_CheckedChanged(object sender, EventArgs e)
+        {
+            lvMain.View = View.SmallIcon;
+        }
+
+        private void rdoBigIcon_CheckedChanged(object sender, EventArgs e)
+        {
+            lvMain.View = View.LargeIcon;
+        }
+
+        private void lvMain_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblText.Text = "";
+
+            ListView.SelectedListViewItemCollection selected = lvMain.SelectedItems;
+
+            foreach(ListViewItem item in selected)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    lblText.Text += $"{item.SubItems[i].Text}   \t";
+                }
+            }
+        }
+    }
+}
